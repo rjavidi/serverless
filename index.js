8cf5a52a2b89f2eebceb3b0bcc4b633148b5acb1
rjavidi <rjavidi@beachbody.com>
2018-05-14 20:46:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ exports.handler = (event, context, callback) => {
 
     // This variable can be updated and checked in to your repository 
     // to update the number of SAM squirrels on the screen.
-    var samCount = 2;
+    var samCount = 500;
 
     // Or you can update your Lambda function's environment variable.
-    var samMultiplier = process.env.SAM_MULTIPLIER || 10;
+    var samMultiplier = process.env.SAM_MULTIPLIER || 2;
 
     var totalSAMs = samCount * samMultiplier;
 
