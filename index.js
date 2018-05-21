78c66a841e4a3d6be54f0ebd07e9f2969301e510
rjavidi <rjavidi@beachbody.com>
2018-05-21 12:21:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.handler = (event, context, callback) => {
 
     callback(null, {
         "statusCode": 200,
-        "body": '"sagi.png"',
+        "body": '"tony.png"',
         "headers":
         {
             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
